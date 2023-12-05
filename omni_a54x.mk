9b4833394233221f4e36db3bdf9803df4414dd09
johnnnyboiy110 <152993245+johnnnyboiy110@users.noreply.github.com>
2023-12-05 15:06:32 +0000

    Update omni_a54x.mk

--- omni_a54x.mk
+++ omni_a54x.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a54x device
 $(call inherit-product, device/samsung/a54x/device.mk)
 
 PRODUCT_DEVICE := a54x
-PRODUCT_NAME := omni_a54x
+PRODUCT_NAME := twrp_a54x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A546B
 PRODUCT_MANUFACTURER := samsung
